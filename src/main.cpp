ad5b93ce54d4ee3b3d80414e8039d321af1b1771
t7o <theaveas.so@gmail.com>
2024-10-19 14:42:02 +0700

    adding imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+#include <raylib-cpp.hpp>
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main() {
-	return 0;
+    raylib::Window window{640, 480, "Boring Canvas"};
+    rlImGuiSetup(true);
+
+    while (!window.ShouldClose()) {
+        window.ClearBackground(GREEN);
+        BeginDrawing();
+        {
+            DrawFPS(10, 10);
+            rlImGuiBegin();
+            ImGui::ShowDemoWindow();
+            rlImGuiEnd();
+        }
+        EndDrawing();
+
+    }
+
+    return 0;
 }
