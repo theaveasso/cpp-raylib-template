5800bacb70d7947c2c878a470ac87a4a9281ea7e
xt8o <theaveas.so@gmail.com>
2024-10-19 14:15:00 +0700

    update content

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,3 @@
-#include <imgui.h>
-#include <raylib-cpp.hpp>
-#include <rlImGui.h>
-
 int main() {
-	raylib::Window window(640, 480, "raylib");
-	rlImGuiSetup(true);
-
-	while (!window.ShouldClose()) {
-		BeginDrawing(); {
-			window.ClearBackground(WHITE);
-			DrawFPS(10, 10);
-		}
-		rlImGuiBegin();
-		ImGui::Begin("Hello world");
-		ImGui::ShowDemoWindow();
-		ImGui::End();
-		rlImGuiEnd();
-		EndDrawing();
-	}
 	return 0;
 }
